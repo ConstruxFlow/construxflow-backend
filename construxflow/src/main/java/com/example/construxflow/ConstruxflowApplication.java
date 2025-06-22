df85c1acd31f717ae8adeefd97fdb6f9b94f0763
ASHAN2021 <akh8130@gmail.com>
2025-06-22 11:24:52 +0530

    floder structure implemented

---
+++ construxflow/src/main/java/com/example/construxflow/ConstruxflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.construxflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstruxflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstruxflowApplication.class, args);
+	}
+
+}
---
+++ construxflow/src/test/java/com/example/construxflow/ConstruxflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.construxflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruxflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
