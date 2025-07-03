5c811837c1d444afa2036fbfb9ecc37bffdeaf47
ASHAN2021 <akh8130@gmail.com>
2025-07-03 13:44:21 +0530

    changes

--- construxflow/src/main/java/com/example/construxflow/service/FirebaseService.java
+++ construxflow/src/main/java/com/example/construxflow/service/FirebaseService.java
@@ -3,7 +3,9 @@ package com.example.construxflow.service;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FirebaseService {
     public FirebaseToken verifyToken(String idToken) throws FirebaseAuthException {
         return FirebaseAuth.getInstance().verifyIdToken(idToken);
