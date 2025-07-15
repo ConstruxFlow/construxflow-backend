a03915c2d6d471d7c2f4f52693f68cc5d821a1ec
Malin Ashmitha Ekanayake <159604336+MalinEkanayake31@users.noreply.github.com>
2025-07-15 19:48:28 +0530

    project creation and material request

---
+++ construxflow/src/main/java/com/example/construxflow/repository/RequestedMaterialRepository.java
@@ -0,0 +1,8 @@
+package com.example.construxflow.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.construxflow.entity.Requested_material;
+
+public interface RequestedMaterialRepository extends JpaRepository<Requested_material, Long> {
+} 
