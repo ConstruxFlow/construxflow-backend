d2a743c0996b520f0f8d23f6c5588204b7f9b1ae
Sandith Moras <2022is061@stu.ucsc.cmb.ac.lk>
2025-07-17 12:47:45 +0530

    equipmnet and inventory mate add done

--- construxflow/src/main/java/com/example/construxflow/controller/EquipmentController.java
+++ construxflow/src/main/java/com/example/construxflow/controller/EquipmentController.java
@@ -4,9 +4,12 @@ import com.example.construxflow.dto.EquipmentDTO;
 import com.example.construxflow.entity.Equipment;
 import com.example.construxflow.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/equipment")
 public class EquipmentController {
@@ -17,5 +20,16 @@ public class EquipmentController {
     @PostMapping("/add")
     public Equipment addEquipment(@RequestBody EquipmentDTO dto) {
         return equipmentService.addEquipment(dto);
+    }
+
+    @GetMapping("/all")
+    public List<Equipment> getAllEquipment() {
+        return equipmentService.getAllEquipment();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Equipment> getEquipmentById(@PathVariable Long id) {
+        Equipment equipment = equipmentService.getEquipmentById(id);
+        return equipment != null ? ResponseEntity.ok(equipment) : ResponseEntity.notFound().build();
     }
 }
