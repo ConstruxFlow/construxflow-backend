8d5420e661fd1c4ba2938e723076572185ab54ff
ASHAN2021 <akh8130@gmail.com>
2025-07-17 09:53:16 +0530

    next schedule part implemented

--- construxflow/src/main/java/com/example/construxflow/dto/EquipmentSchedulingRequestDTO.java
+++ construxflow/src/main/java/com/example/construxflow/dto/EquipmentSchedulingRequestDTO.java
@@ -14,6 +14,8 @@ public class EquipmentSchedulingRequestDTO {
     private String id;
     private String equipmentType;
     private String equipmentName;
+    private String maintenanceType;
+    private String priority;
     private Date date;
     private Time time;
     private String description;
