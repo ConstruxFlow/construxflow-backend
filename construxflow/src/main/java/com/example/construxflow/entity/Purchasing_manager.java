366df0cbb4459463e2215844643cf3a673eae15b
NIHARA W.A.S <sandarunihara56@gmail.com>
2025-07-19 16:15:04 +0530

    Add material_req_id and projectId fields to DTOs and entities; implement HTML email sending in UserService

--- construxflow/src/main/java/com/example/construxflow/entity/Purchasing_manager.java
+++ construxflow/src/main/java/com/example/construxflow/entity/Purchasing_manager.java
@@ -18,7 +18,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Purchasing_manager extends Manager {
 
-    @OneToMany(mappedBy = "manager")
-    private List<Quotation_request> quotationRequests;
+
 
 }
