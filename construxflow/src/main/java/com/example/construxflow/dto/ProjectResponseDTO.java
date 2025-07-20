3b3159c153ee4392736422d015140a5c5c6a6423
Malin Ashmitha Ekanayake <159604336+MalinEkanayake31@users.noreply.github.com>
2025-07-20 19:00:20 +0530

    site manager id

--- construxflow/src/main/java/com/example/construxflow/dto/ProjectResponseDTO.java
+++ construxflow/src/main/java/com/example/construxflow/dto/ProjectResponseDTO.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 public class ProjectResponseDTO {
     private String projectId;
+    private String managerId;
     private String projectName;
     private String location;
     private String startDate;
