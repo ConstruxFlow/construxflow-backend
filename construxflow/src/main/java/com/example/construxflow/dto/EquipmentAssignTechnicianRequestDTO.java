33196f4e6f7688f6ef84ecb6ea4d92042f2a4f41
ASHAN2021 <akh8130@gmail.com>
2025-07-21 19:41:32 +0530

    new changes

--- construxflow/src/main/java/com/example/construxflow/dto/EquipmentAssignTechnicianRequestDTO.java
+++ construxflow/src/main/java/com/example/construxflow/dto/EquipmentAssignTechnicianRequestDTO.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EquipmentAssignTechnicianRequestDTO {
     private String equipmentSchedulingId;
-    private String technicianId;
+    private List<String> technicianIds;
     private String duration;
     private String notes;
     private String startDate;
